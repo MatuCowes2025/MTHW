551b30741763048e01eb82f2b5c2c87b137fb175
MatuCowes2025 <matucowes@gmail.com>
2025-05-05 22:02:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () { 
+document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.querySelector('.nav__toggle');
     const menu = document.querySelector('.nav__link');
 
+    
     toggleButton.addEventListener('click', function () {
-        menu.classList.toggle('show');
+        menu.classList.toggle('nav__link--show');
     });
 });
-

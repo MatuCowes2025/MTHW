ff5e78f8bb84ce220bc0c242e1b26235da401956
MatuCowes2025 <matucowes@gmail.com>
2025-05-05 22:04:49 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.nav__toggle');
-    const menu = document.querySelector('.nav__link');
-
-    
-    toggleButton.addEventListener('click', function () {
-        menu.classList.toggle('nav__link--show');
-    });
-});
